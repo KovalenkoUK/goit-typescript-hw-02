6c70f8cc4b02fd64f43f9c44bed6e88ec2b80887
Matvey Kovalenko <kovalenko.matvey96@gmail.com>
2025-05-09 22:31:24 +0300

    1

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import "modern-normalize";
+import "./index.css";
+import React, { StrictMode } from "react";
+import App from "./App";
+import { createRoot } from "react-dom/client";
+
+createRoot(document.getElementById("root") as HTMLElement).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
